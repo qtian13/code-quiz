3d636bebd59a5d35d27d7fec8f4e07859032c62e
qtian13 <qiushuang.tian@gmail.com>
2021-06-27 20:55:24 -0700

    finish clear score function   did code refactory

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var currentTimer;
 var currentInterval;
 var currentTimeLeft = 0;
 var timePenalty = 5;
+var validInitial = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var question0 = {
     questionDescr : "what is my number?",
     options: ["1", "2", "3", "4"],
@@ -19,119 +20,78 @@ var questions = [question0, question1];
 var timeTotal = timePenalty * questions.length;
 
 var topBarSnippet = 
-        '<div id="highscore">' +
-            'View Highscores' +
-        '</div>' +
-        '<div id="timeleft">' +
-            'Time: 0' +
-        '</div>';
+        '<div id="highscore">View Highscores</div>' +
+        '<div id="timeleft">Time left: 0</div>';
 
 var homepageSnippet = 
-        '<div class="homepage-content">' +
-            '<h1>Coding Quiz Challenge</h1>' +
-            '<p class="rules">Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ' + timePenalty + ' seconds!</p>' +
-            '<button id="startbutton">' +
-                'Start Quiz' +
-            '</button>' +
-        '</div>';
+        '<h1>Coding Quiz Challenge</h1>' +
+        '<p class="rules">Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ' + timePenalty + ' seconds!</p>' +
+        '<button id="startbutton">Start Quiz</button>';
+
 var resultpageSnippet =
-        '<h3>All done!</h3>' +
+        '<h2>All done!</h2>' +
         '<div id="score">Your final score is </div>' +
-        '<div class="submit">' +
-            'Enter Initials: <textarea name="initials" id="initials" cols="30" rows="1"></textarea><button id="submit-score">submit</button>' +
-        '</div>';
+        '<form class="submit">' +
+            'Enter Initials: ' + 
+            '<input id="initials"></textarea>' +
+            '<button id="submit-score">submit</button>' +
+        '</form>';
 
 var highscoreSnippet = 
-        '<div id="highscore-box">' +
-            '<h3>Highscores</h3>' +
-            '<div id="highscore-list">' +
-            '</div>' +
-            '<button id="to-homepage">Go Back</button>' +
-            '<button id="clear-highscore">Clear Highscores</button>' +
-        '</div>'
+        '<h2>Highscores</h2>' +
+        '<ol id="highscore-list"></ol>' +
+        '<button id="to-homepage">Go Back</button>' +
+        '<button id="clear-highscore">Clear Highscores</button>';
 
 function loadHomepage() {
     topBar.innerHTML = topBarSnippet;
-    mainContent.setAttribute("style", "justify-content: center");
     mainContent.innerHTML = homepageSnippet;
 }
 
 function selectPosition(event) {
     var element = event.target;
     if (element.matches("#startbutton")) {
-        clearContent();
         startQuiz();
     } else if (element.matches(".option")) {
-        var result = document.querySelector(".question-result");
-        result.setAttribute("style", "border-top: 2px solid black; width: 100%; margin-top: 8px");
+        var result = displayResult();
         if (element.dataset.ans == "true") {
             result.textContent = "Correct!"
         } else {
             result.textContent = "Incorrect!"
-            if (currentTimeLeft > timePenalty) {
-                currentTimeLeft -= timePenalty;
-                document.querySelector("#timeleft").textContent = "time: " + currentTimeLeft;
-                setTimer(currentTimeLeft);
-            } else {
-                setTimer(0.01);
-                document.querySelector("#timeleft").textContent = "time: " + 0;
-                currentTimeLeft = 0;
+            if (!setNewTimer(currentTimeLeft - timePenalty)) {
+                loadResultPage();
                 return;
             }
         }
-        setTimeout(function () {
-            result.textContent = "";
-            result.setAttribute("style", "border-top: none");
-        }, 1000 * 1);
-        currentQuestionIndex++;
-        document.querySelector(".question-box").innerHTML = "";
-        if (currentQuestionIndex == questions.length) {
-            loadResultPage();
-        } else {
-            loadNewQuestion();
-        }
-
+        generateNextQuestion();
     } else if (element.matches("#highscore")) {
-        console.log("I am executed");
-        mainContent.innerHTML = "";
         loadHighScores();
     } else if (element.matches("#submit-score")) {
-        var initials = document.querySelector("#initials").value.trim();
-        if (initials.length != 2) {
-            alert("Please enter correct initials of your first name and last name without space!");
-            return;
+        event.preventDefault();
+        if (createNewScore()) {
+            updateScoreRecord(newScore);
+            currentQuestionIndex = 0;
+            mainContent.innerHTML = "";
+            loadHighScores();
         }
-        var newScore = {
-            name: initials,
-            score: currentTimeLeft,
-        };
-        console.log(newScore);
-        updateScoreRecord(newScore);
-        console.log(localStorage.getItem("scores"));
-        currentQuestionIndex = 0;
-        mainContent.innerHTML = "";
-        loadHighScores();
     } else if (element.matches("#to-homepage")) {
-        mainContent.innerHTML = "";
         loadHomepage();
-    } else if (element.matches("#clear-highscore")) {
+    } 
+    else if (element.matches("#clear-highscore")) {
         localStorage.removeItem("scores");
         loadHighScores();
     }
 }
 
-function clearContent() {
-    mainContent.innerHTML = "";
-}
-
 function startQuiz() {
     currentTimeLeft = timeTotal;
-    document.querySelector("#timeleft").textContent = "time: " + currentTimeLeft;
+    document.querySelector("#timeleft").textContent = "Time left: " + currentTimeLeft;
     currentInterval = setInterval(function() {
         currentTimeLeft--;
-        document.querySelector("#timeleft").textContent = "time: " + currentTimeLeft;
+        document.querySelector("#timeleft").textContent = "Time left: " + currentTimeLeft;
     }, 1000 * 1);
     setTimer(currentTimeLeft);
+    setNewTimer(currentTimeLeft);
     createQuestionContainer();
     loadNewQuestion();
 }
@@ -139,9 +99,9 @@ function startQuiz() {
 function createQuestionContainer() {
     var questionDisplay = document.createElement("div");
     var questionResultDisplay = document.createElement("div");
-    mainContent.setAttribute("style", "justify-content: flex-start");
     questionDisplay.setAttribute("class", "question-box");
     questionResultDisplay.setAttribute("class", "question-result");
+    mainContent.innerHTML = "";
     mainContent.appendChild(questionDisplay);
     mainContent.appendChild(questionResultDisplay);
 }
@@ -149,7 +109,7 @@ function createQuestionContainer() {
 function loadNewQuestion() {
     var currentQuestion = questions[currentQuestionIndex];
     var questionBox = document.querySelector(".question-box");
-    var question = document.createElement("h3");
+    var question = document.createElement("h2");
     var options = document.createElement("div");
     questionBox.appendChild(question);
     questionBox.appendChild(options);
@@ -163,17 +123,11 @@ function loadNewQuestion() {
     }
 }
 
-function loadTimeOutPage() {
-    clearInterval(currentInterval);
-    clearTimeout(currentTimer);
-    mainContent.innerHTML = "TIME OUT!";
-}
-
 function loadResultPage() {
     clearInterval(currentInterval);
     clearTimeout(currentTimer);
     var box = document.querySelector(".question-box");
-    box.setAttribute("class", "question-box result-box");
+    box.setAttribute("class", "result-box");
     box.innerHTML = resultpageSnippet;
     var score = document.querySelector("#score");
     score.textContent += currentTimeLeft + "!";
@@ -181,9 +135,10 @@ function loadResultPage() {
 
 function setTimer(timeLength) {
     clearTimeout(currentTimer);
+    timeLength = timeLength == 0 ? 0.1 : timeLength;
     currentTimer = setTimeout(function() {
         clearInterval(currentInterval);
-        loadTimeOutPage();
+        loadResultPage();
     }, 1000 * timeLength);
 }
 
@@ -193,11 +148,12 @@ function loadHighScores() {
     topBar.innerHTML = "";
     mainContent.innerHTML = highscoreSnippet;
     scoreList = document.querySelector("#highscore-list")
+    scoreList.setAttribute("style", "padding-left: 20px");
+
     for (var i = 0; i < scores.length; i++) {
-        var score = document.createElement("div");
-        score.textContent = (i + 1) + ". " + scores[i].name + " " + scores[i].score;
+        var score = document.createElement("li");
+        score.textContent = scores[i].name + " " + scores[i].score;
         scoreList.appendChild(score);
-        score.setAttribute("style", "width: 100%");
     }
 }
 
@@ -205,7 +161,6 @@ function updateScoreRecord(newItem) {
     var scores = [];
     var index = 0;
     if (localStorage.getItem("scores")) {
-        console.log("localStorage scores not empty")
         scores = JSON.parse(localStorage.getItem("scores"));
     }
     for (; index < scores.length; index++) {
@@ -215,6 +170,46 @@ function updateScoreRecord(newItem) {
     }
     scores.splice(index, 0, newItem);
     localStorage.setItem("scores", JSON.stringify(scores));
+}
+
+function displayResult() {
+    var result = document.querySelector(".question-result");
+    result.setAttribute("style", "border-top: 2px solid black; width: 100%; margin-top: 8px");
+    setTimeout(function () {
+        result.textContent = "";
+        result.setAttribute("style", "border-top: none");
+    }, 1000 * 1);
+    return result;
+}
+
+function setNewTimer(time) {
+    var noTimeLeft = time <= 0;
+    currentTimeLeft = noTimeLeft? 0 : time;
+    document.querySelector("#timeleft").textContent = "Time left: " + currentTimeLeft;
+    setTimer(currentTimeLeft);
+    return !noTimeLeft;
+}
+
+function generateNextQuestion() {
+    currentQuestionIndex++;
+    document.querySelector(".question-box").innerHTML = "";
+    if (currentQuestionIndex == questions.length) {
+        loadResultPage();
+    } else {
+        loadNewQuestion();
+    }
+}
+
+function createNewScore() {
+    var initials = document.querySelector("#initials").value.trim().toUpperCase();
+    if (initials.length != 2 || validInitial.indexOf(initials[0]) < 0 || validInitial.indexOf(initials[1]) < 0) {
+        alert("Please enter correct initials of your first name and last name without space!");
+        return null;
+    }
+    return newScore = {
+        name: initials,
+        score: currentTimeLeft,
+    };
 }
 
 loadHomepage();
